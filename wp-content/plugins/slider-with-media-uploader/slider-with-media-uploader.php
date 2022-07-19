5ed9b584f3e1e0a3264f8a2439b62e7d3c19eb1f
habray <parbatlimbu4@gmail.com>
2022-07-19 20:11:05 +0545

    add heading and sub heading caption

--- wp-content/plugins/slider-with-media-uploader/slider-with-media-uploader.php
+++ wp-content/plugins/slider-with-media-uploader/slider-with-media-uploader.php
@@ -48,41 +48,70 @@ class SliderWithMedaiUploader
   // createHTML function for HTML code
   function createHTML()
   {
-    $html = '<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-            </div>
-            <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="' . get_option('wcp_image1') . '" class="d-block w-100" alt="img1">
-          </div>
-          <div class="carousel-item">
-            <img src="' . get_option('wcp_image2') . '" class="d-block w-100" alt="img2">
-          </div>
-          <div class="carousel-item">
-            <img src="' . get_option('wcp_image3') . '" class="d-block w-100" alt="img3">
-          </div>
-          <div class="carousel-item">
-            <img src="' . get_option('wcp_image4') . '" class="d-block w-100" alt="img4">
-          </div>
-          <div class="carousel-item">
-            <img src="' . get_option('wcp_image5') . '" class="d-block w-100" alt="img5">
-          </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>';
+    $html = 
+  '<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img src="' . get_option('wcp_image1') . '" class="d-block w-100" alt="img1">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>' . esc_html(get_option('wcp_caption1', 'First slide label')) . '</h5>
+        <p>' . esc_html(get_option('wcp_subcaption1', 'First slide label')) . '</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="' . get_option('wcp_image2') . '" class="d-block w-100" alt="img2">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="' . get_option('wcp_image3') . '" class="d-block w-100" alt="img3">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+    <img src="' . get_option('wcp_image4') . '" class="d-block w-100" alt="img4">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+    <img src="' . get_option('wcp_image5') . '" class="d-block w-100" alt="img5">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>';
     return $html;
+  }
+
+  // add settings field and register field
+  function addSettings($cpt_settings){
+    add_settings_field('wcp_image1', "Image 1", array($this, 'imageHTML'), 'img-slider-settings-page', 'wcp_first_section', array('imgName' => 'wcp_image1', 'previewimgClass' => 'img1'));
+    register_setting('sliderplugin', 'wcp_image1', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'https://shankerdevcampus.edu.np/assets/site/images/no-image.jpg'));    
   }
  
   // admin setting page
@@ -110,7 +139,20 @@ class SliderWithMedaiUploader
     // For Image 5
     add_settings_field('wcp_image5', "Image 5", array($this, 'imageHTML'), 'img-slider-settings-page', 'wcp_first_section', array('imgName' => 'wcp_image5', 'previewimgClass' => 'img5'));
     register_setting('sliderplugin', 'wcp_image5', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'https://shankerdevcampus.edu.np/assets/site/images/no-image.jpg'));
+
+    // Caption Heading 1
+    add_settings_field('wcp_caption1', 'Image 1 Caption Heading', array($this, 'captionHTML'), 'img-slider-settings-page', 'wcp_first_section', array('caption' => 'wcp_caption1'));
+    register_setting('sliderplugin', 'wcp_caption1', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Your Caption Here'));
+
+    // Caption Sub Heading 1
+    add_settings_field('wcp_subcaption1', 'Image 1 Caption Sub Heading', array($this, 'captionHTML'), 'img-slider-settings-page', 'wcp_first_section', array('caption' => 'wcp_subcaption1'));
+    register_setting('sliderplugin', 'wcp_subcaption1', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Your Sub Caption Here'));
   }
+
+  // Caption HTML
+  function captionHTML($arg){ ?>
+    <input type="text" name="<?php echo $arg['caption'] ?>" value="<?php echo esc_attr(get_option($arg['caption'])) ?>">
+  <?php }
  
   // Image selection HTML
   function imageHTML($arg)
