9c5c743d94a1885d0057a5f38755f5cff3a6d5b9
habray <parbatlimbu4@gmail.com>
2022-07-20 20:16:41 +0545

    using namespace

---
+++ wp-content/plugins/Lesson9/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nerd_platoon/lesson9',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5ed9b584f3e1e0a3264f8a2439b62e7d3c19eb1f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nerd_platoon/lesson9' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5ed9b584f3e1e0a3264f8a2439b62e7d3c19eb1f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
