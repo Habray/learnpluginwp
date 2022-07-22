aee6b683be823f807fe9bd21c003ee3259ede02c
habray <parbatlimbu4@gmail.com>
2022-07-22 17:37:52 +0545

    Dynamic Admin SubPages added

--- wp-content/plugins/Lesson9/inc/Api/SettingsApi.php
+++ wp-content/plugins/Lesson9/inc/Api/SettingsApi.php
@@ -9,6 +9,8 @@ class SettingsApi{
 
     public $admin_pages = array();
 
+    public $admin_subpages = array();
+
     /**
      * we use register() function to register somthing or initialize a class or trigger the class
      * And use __construct() to only update variables and define some unique specification of the class
@@ -28,7 +30,38 @@ class SettingsApi{
     public function addPages(array $pages){
         $this->admin_pages = $pages;
 
-        // for method chaning 
+        // for method chaining 
+        return $this;
+    }
+
+    public function withSubPage(string $title =null){
+        if(empty($this->admin_pages)){
+            return $this;
+        }
+
+        $admin_page = $this->admin_pages[0];
+
+        $subpage = [
+            [
+                'parent_slug' => $admin_page['menu_slug'],
+                'page_title' => $admin_page['page_title'],
+                'menu_title'  => ($title) ? $title : $admin_page['menu_title'],
+                'capability'  => $admin_page['capability'],
+                'menu_slug'  => $admin_page['menu_slug'],
+                'callback'  => $admin_page['callback'],
+            ]
+        ];
+
+        $this->admin_subpages = $subpage;
+
+        return $this;
+    }
+
+    // Dynamically generate subPage
+    public function addSubPages(array $subpage){
+        $this->admin_subpages = array_merge($this->admin_subpages ,$subpage);
+
+        // for method chaining
         return $this;
     }
 
@@ -36,8 +69,15 @@ class SettingsApi{
      * here we loop though all the pages that we pass through our AddPages() and trigger the default the add menu page
      */
     public function addAdminMenu(){
+
+        // for admin menu page
         foreach($this->admin_pages as $page){
             add_menu_page($page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position']);
+        }
+
+        // for admin sub menu page
+        foreach($this->admin_subpages as $page){
+            add_submenu_page($page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback']);
         }
     }
 }
--- wp-content/plugins/Lesson9/inc/Pages/Admin.php
+++ wp-content/plugins/Lesson9/inc/Pages/Admin.php
@@ -12,6 +12,7 @@ class Admin extends BaseController{
 
     public $settings;
     public $pages;
+    public $subpages;
 
     public function __construct()
     {
@@ -28,17 +29,20 @@ class Admin extends BaseController{
             'position'  => '110'
             ]
         ];
+
+        $this->subpages = [
+            [
+                'parent_slug' => 'alecaddd_plugin',
+                'page_title' => 'Custom Post Types',
+                'menu_title'  => 'CPT',
+                'capability'  => 'manage_options',
+                'menu_slug'  => 'alecaddd_cpt',
+                'callback'  => function(){echo '<h1> CPT Manager</h1>';},
+            ]
+        ];
     }
 
     public function register(){
-        $this->settings->addPages($this->pages)->register();
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
-
-    // public function add_admin_pages() {
-    //     add_menu_page( 'Alecaddd Plugin', 'Alecaddd', 'manage_options', 'alecaddd_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-    // }
-
-    // public function admin_index() {
-    //     require_once $this->plugin_path . 'templates/admin.php';
-    // }
 }
