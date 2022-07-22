9ea3ba3b41d4e981d737d07340bddbe17c710415
habray <parbatlimbu4@gmail.com>
2022-07-22 14:17:12 +0545

    FIXED Notice: ob_end_flush(): failed to send buffer of zlib output compression (0)

--- wp-content/themes/twentytwenty/functions.php
+++ wp-content/themes/twentytwenty/functions.php
@@ -35,6 +35,8 @@
  */
 function twentytwenty_theme_support() {
 
+	remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
